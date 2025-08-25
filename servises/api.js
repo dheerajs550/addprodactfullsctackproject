df144a564c6eb50daa47b4b9657e83c5b62779ef
Dheeraj sharma <dheerajsharma550@gmail.com>
2025-08-25 13:20:04 +0530

    new code

--- servises/api.js
+++ servises/api.js
@@ -2,7 +2,7 @@ import { BASE_URL } from "@/lib/apiurl";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`, // backend ka base url
+  baseURL: `${BASE_URL}`, // backend ka base url
   headers: {
     "Content-Type": "application/json",
   },
